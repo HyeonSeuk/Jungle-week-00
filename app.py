45ab04775ce2dfa796b368c25a65a7ec38475e65
HyeonSeuk <seo5719@naver.com>
2023-08-07 21:51:20 +0900

    login

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bson import ObjectId
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://test:test@3.34.94.133',27017)
+client = MongoClient('localhost',27017)
 db = client.dbjungle
 
 
@@ -13,6 +13,13 @@ db = client.dbjungle
 def home():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
