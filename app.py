0b73e239367d501fb4dbdebdb2d3fcb182c1ed41
Richard Shin <qudcjf153@gmail.com>
2023-08-07 20:09:55 +0900

    feat(app.py): 회원가입 라우팅 추가
    반환값은 string으로 'register'로 임시로 저장함

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ db = client.dbjungle
 @app.route('/')
 def home():
     return render_template('index.html')
-
+    
+@app.route('/register')
+def register():
+    return 'register'
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
