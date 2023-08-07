ff854695b7139f327c9210ad357e465509d41215
cece <sojeong.lee017@gmail.com>
2023-08-08 01:43:38 +0900

    tailwindcss로 변경, index.html, base.html UI구성

--- app.py
+++ app.py
@@ -4,28 +4,34 @@ from bson import ObjectId
 
 app = Flask(__name__)
 
-client = MongoClient('localhost',27017)
+client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
 
-# HTML 화면 보여주기 
+# HTML 화면 보여주기
 @app.route('/')
 def home():
     all_events = list(db.user.find({}))
-    print(all_events)
-    return render_template('index.html', template_events= all_events)
+    for e in all_events:
+        e['_id'] = str(e['_id'])
+    # print(all_events)
+    return render_template('index.html', template_events=all_events)
+
 
 @app.route('/favorite/<user_id>')
 def favorite(user_id):
     return render_template('index.html')
-    
-@app.route('/register')
+
+
+@app.route('/signup')
 def register():
-    return 'register'
+    return 'signup'
+
 
 @app.route('/login')
 def login():
     return 'login'
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
