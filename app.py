a0f94a4d77947fd2b116ed345d1d6571b339bf28
Richard Shin <qudcjf153@gmail.com>
2023-08-07 20:13:31 +0900

    feat(app.py): 로그인 라우팅 추가
    반환값은 'login'으로 임시저장

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ def home():
 def register():
     return 'register'
 
+@app.route('/login')
+def login():
+    return 'login'
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
