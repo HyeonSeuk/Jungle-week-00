2ff11c0edbc3dc57e431465514df89d048cd86d9
c4fiber <qudcjf153@gmail.com>
2023-08-08 17:44:11 +0900

    feat(signup): 회원가입 검증과정 추가 (#11)
    * feat(signup): 비밀번호, 비밀번호 확인 검증과정 추가
    
    1. flask(backend)에서 비밀번호, 비밀번호 확인 둘 다 전달받고 검증하는 과정을 추가함
    - 일치하지 않을 경우 flash를 통해 메세지를 전달함.
    - 일치할 경우 login page로 리다이렉트함
    2. flash 기능을 사용하기 위해 app.secret_key를 설정함
    
    * feat(favorite): 사용자가 좋아요 표시한 카드만 출력하는 페이지 기능 구현
    -> * fix(favorite): 필요없어져서 제거함

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, jsonify, redirect, url_for, request
+from flask import Flask, render_template, jsonify, redirect, url_for, request, flash
 from bson import ObjectId
 import requests, hashlib
 
 app = Flask(__name__)
+app.secret_key = 'jungle7'
 
 client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
 
-# HTML 화면 보여주기
+## 메인 페이지
 @app.route('/')
 def home():
     # arg로 전달된 페이징을 확인, 없으면 1
@@ -17,29 +18,36 @@ def home():
     all_events = get_all_events()
     return render_template('index.html', template_events= all_events, pageNo=page)
 
+## 회원가입 페이지
+# form 입력(nickname, email, pwd, pwd2를 전달받는다.)
+# 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    if request.method == 'GET':
-        return render_template('signup.html')
-    else:
+    if request.method == 'POST':
         nickname = request.form['nickname']
         email = request.form['email']
-        password = request.form['password']
+        pwd = request.form['password']
+        pwd_confirm = request.form['password2']
 
-        pw_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        # 확인 pwd가 일치하지 않으면 에러메시지와 함께 [GET]'/signup'으로 리다이렉트
+        if pwd != pwd_confirm:
+            flash('비밀번호와 확인 비밀번호가 일치하지 않습니다.', 'error')
+            return redirect(url_for('signup'))
 
-        db.users.insert_one({'nickname':nickname, 'email':email, 'password':pw_hash})
+        # pwd암호화 후 저장
+        pwd_hash = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
+        db.users.insert_one({'nickname':nickname, 'email':email, 'password':pwd_hash})
         
-        return redirect(url_for('login', msg="회원가입에 성공하였습니다."))
+        return redirect(url_for('login'))
+    
+    return render_template('signup.html') 
 
+## 로그인 페이지
 @app.route('/login')
 def login():
     return render_template('login.html')
-
-@app.route('/favorite/<user_id>')
-def favorite(user_id):
-    return render_template('index.html')
     
+## 대구광역시 open API에 접근해서 데이터를 가져오는 페이지
 @app.route('/refresh')
 def refresh():
     url = f'http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=HF37SOzpRH8DBXxqviNM%2FxjayRLamasAPu7bsT%2F6hu5cK6KT4hRkoQAUVFJOqRxnpjBW4MZMNa5XCMIWRMDnPg%3D%3D'
