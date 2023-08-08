8ee3cf379ed7620b52eb339d34b3dbb108e4586b
Richard Shin <qudcjf153@gmail.com>
2023-08-08 17:25:29 +0900

    feat(favorite): 사용자가 좋아요 표시한 카드만 출력하는 페이지 기능 구현

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
 
-# HTML 화면 보여주기
+## 메인 페이지
 @app.route('/')
 def home():
     all_events = list(db.events.find({}))
     print(all_events)
     return render_template('index.html', template_events= all_events)
 
+## 회원가입 페이지
+# form 입력(nickname, email, pwd, pwd2를 전달받는다.)
+# 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
@@ -25,10 +28,12 @@ def signup():
         pwd = request.form['password']
         pwd_confirm = request.form['password2']
 
+        # 확인 pwd가 일치하지 않으면 에러메시지와 함께 [GET]'/signup'으로 리다이렉트
         if pwd != pwd_confirm:
             flash('비밀번호와 확인 비밀번호가 일치하지 않습니다.', 'error')
             return redirect(url_for('signup'))
 
+        # pwd암호화 후 저장
         pwd_hash = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
         db.users.insert_one({'nickname':nickname, 'email':email, 'password':pwd_hash})
         
@@ -36,14 +41,19 @@ def signup():
     
     return render_template('signup.html') 
 
+## 로그인 페이지
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+## 특정 사용자의 좋아요 표시한 카드만 출력해주는 페이지
 @app.route('/favorite/<user_id>')
 def favorite(user_id):
-    return render_template('index.html')
+    users_events = list(db.events.find({'_id':user_id}))
+    print(users_events)
+    return render_template('index.html', template_events=users_events)
     
+## 대구광역시 open API에 접근해서 데이터를 가져오는 페이지
 @app.route('/refresh')
 def refresh():
     url = f'http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=HF37SOzpRH8DBXxqviNM%2FxjayRLamasAPu7bsT%2F6hu5cK6KT4hRkoQAUVFJOqRxnpjBW4MZMNa5XCMIWRMDnPg%3D%3D'
